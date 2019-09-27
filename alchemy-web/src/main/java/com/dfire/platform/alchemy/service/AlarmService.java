ca581ff59e321b59de85d2942ba4f35642b42de3
congbai <congbai@2dfire.com>
2019-09-27 11:17:27 +0800

    修改告警内容

--- alchemy-web/src/main/java/com/dfire/platform/alchemy/service/AlarmService.java
+++ alchemy-web/src/main/java/com/dfire/platform/alchemy/service/AlarmService.java
@@ -1,22 +1,51 @@
 package com.dfire.platform.alchemy.service;
 
+import io.github.jhipster.config.JHipsterConstants;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Service
 public class AlarmService {
 
+    private static final String DATE_FORMMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL = ThreadLocal.withInitial(() -> {
+        DateFormat df = new SimpleDateFormat(DATE_FORMMAT);
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        return df;
+    });
+
     private final DingTalkService dingTalkService;
 
-    public AlarmService(DingTalkService dingTalkService) {
+    private final boolean prod;
+
+    public AlarmService(Environment env,  DingTalkService dingTalkService) {
         this.dingTalkService = dingTalkService;
+        if (env != null && env.acceptsProfiles(Profiles.of(JHipsterConstants.SPRING_PROFILE_PRODUCTION))) {
+            prod = true;
+        } else {
+            prod = false;
+        }
     }
 
-    public void alert(String businessName, String jobName){
-        dingTalkService.sendMessage("flink任务失败", String.format("#### 状态：失败 \n #### 业务：%s \n #### 任务：%s", businessName,  jobName));
+    public void alert(String businessName, String jobName) {
+        if(!prod){
+            return;
+        }
+        dingTalkService.sendMessage("flink任务失败", String.format("#### 状态：失败 \n #### 业务：%s \n #### 任务：%s \n #### 时间：%s", businessName, jobName, DATE_FORMAT_THREAD_LOCAL.get().format(new Date())));
     }
 
-    public void recover(String businessName, String jobName){
-        dingTalkService.sendMessage("flink任务恢复", String.format("#### 状态：恢复 \n ### 业务：%s \n #### 任务：%s", businessName,  jobName));
+    public void recover(String businessName, String jobName) {
+        if(!prod){
+            return;
+        }
+        dingTalkService.sendMessage("flink任务恢复", String.format("#### 状态：恢复 \n #### 业务：%s \n #### 任务：%s \n #### 时间：%s", businessName, jobName, DATE_FORMAT_THREAD_LOCAL.get().format(new Date())));
     }
 
 }
